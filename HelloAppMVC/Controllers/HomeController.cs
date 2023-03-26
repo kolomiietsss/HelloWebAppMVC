cd2b9e4784a29d035f1e4292f59fe4977dd010e8
Artem Kolomiiets <akolomiets02@gmail.com>
2023-03-26 22:21:47 +0300

    GG

---
+++ HelloAppMVC/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using HelloAppMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HelloAppMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public IActionResult SayHelloViewBag(string name)
+        {
+            ViewBag.Message = name;
+            return View("Index");
+        }
+
+        public IActionResult SayHelloContent(string name)
+        {
+            string message = "Здарова, " + name + "!";
+            return Content(message);
+        }
+
+        public IActionResult SayHelloView(string name)
+        {
+            ViewBag.Message = name;
+            return View("View");
+        }
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
